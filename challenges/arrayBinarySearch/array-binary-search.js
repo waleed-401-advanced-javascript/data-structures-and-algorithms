68f97e227cea4a9c18fe63bc5f3f6145d7cf9250
waleedfarraj <waleed.farraj95@gmail.com>
2020-09-09 01:51:16 +0300

    done

---
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -0,0 +1,36 @@
+'use strict'
+
+function binarySearch(arr, key){
+    let i = 0;
+    let j = arr.length;
+    let matching = -1
+    let midpoint = Math.ceil((j+i)/2);
+    while ((midpoint!==i)&&(midpoint!==j)&&(matching === -1) ){
+    let matchCheck = arr[midpoint];
+    console.log(matchCheck)
+    console.log(midpoint)
+    console.log('run')
+      if(matchCheck === key){
+        matching = midpoint;
+        return matching;
+      } else if (matchCheck < key){
+        i = midpoint;
+        midpoint = Math.ceil((j+i)/2);
+      } else if (matchCheck > key){
+        j = midpoint;
+        midpoint = Math.ceil((j+i)/2);
+        console.log(midpoint)
+        console.log('end')
+    }
+    
+    }
+  
+    if (key == arr[0]){
+      matching = 0}
+  
+  
+    return matching;
+  }
+
+
+  
