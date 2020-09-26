a3939d4a74498a06163e341feef4b65f06662fb3
waleedfarraj <waleed.farraj95@gmail.com>
2020-09-27 01:06:08 +0300

    tests added

---
+++ __test__/multibracket.test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const multiBracketValidation = require('../challenges/multiBracketValidation/multi-bracket-validation');
+
+describe('multi bracked validation class',()=>{
+  it ('should return true in this case', () => {
+    expect(multiBracketValidation('[]')).toBeTruthy();
+  });
+  it ('should return true in this case', () => {
+    expect(multiBracketValidation('[[]]')).toBeTruthy();
+  });
+  it ('should return true in this case', () => {
+    expect(multiBracketValidation('{}')).toBeTruthy();
+  });
+  it ('should return true in this case', () => {
+    expect(multiBracketValidation('()')).toBeTruthy();
+  });
+  it ('should return false in this case', () => {
+    expect(multiBracketValidation('(')).toBeFalsy();
+  });
+  it ('should return false in this case', () => {
+    expect(multiBracketValidation('{')).toBeFalsy();
+  });
+  it ('should return false in this case', () => {
+    expect(multiBracketValidation('[')).toBeFalsy();
+  });
+  it ('should return false in this case', () => {
+    expect(multiBracketValidation('[{]}')).toBeFalsy();
+  });
+//   it ('should return false in this case', () => {
+//     expect(multiBracketValidation(']')).toBeFalsy();
+//   });
+//   it ('should return false in this case', () => {
+//     expect(multiBracketValidation('}')).toBeFalsy();
+//   });
+//   it ('should return false in this case', () => {
+//     expect(multiBracketValidation(')')).toBeFalsy();
+//   });
+  it ('should return false in this case', () => {
+    expect(multiBracketValidation('[}')).toBeFalsy();
+  });
+  it ('should return false in this case', () => {
+    expect(multiBracketValidation('[)')).toBeFalsy();
+  });
+  it ('should return false in this case', () => {
+    expect(multiBracketValidation('{]')).toBeFalsy();
+  });
+  it ('should return false in this case', () => {
+    expect(multiBracketValidation('([')).toBeFalsy();
+  });
+  it ('should return false in this case', () => {
+    expect(multiBracketValidation('[(')).toBeFalsy();
+  });
+
+});
